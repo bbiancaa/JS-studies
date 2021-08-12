8c24b3e4f5859016ca7a281e92832300bf557762
bbiancaa <biancagabriela.fritsch@hotmail.com>
2021-08-12 09:10:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//JS é uma linguagem fracamente tipada e dinâmica
+/*Variáveis:
+    var -> global e poderá funcionar fora do escopo de um bloco
+    const e let são locais e só funcionam no escopo onde foram criad
+*/
