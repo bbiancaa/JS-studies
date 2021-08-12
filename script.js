732c8f723345633752fead3b7053cd17529a7399
bbiancaa <biancagabriela.fritsch@hotmail.com>
2021-08-12 09:41:17 -0300

    conceito de hoisting, scope, const, let

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
     var -> global e poderá funcionar fora do escopo de um bloco
     const e let são locais e só funcionam no escopo onde foram criad
 */
+let y =1;
+
+{
+    y = 0
+    console.log('valor de y eh', y)
+}
+
+console.log('agora o valor de y eh ', y)
+//saida: valor de y eh = 0
+//       agora o valor de y eh = 0
+//isso acontece pq o y ta sobrescrevendo o let y, já que no escopo o y não esta usando var. reservada
+//isso eh chamado de hoisting
